3d03f14abb92fb862eadd6c460646e7efd0e5507
Filip Pranklin <filip.pranklin@gmail.com>
2020-03-11 01:49:26 +0100

    implement OTA updates

--- mqtt_esp8266/mqtt_esp8266.ino
+++ mqtt_esp8266/mqtt_esp8266.ino
@@ -1,10 +1,14 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
-const char* ssid = "[redacted]";
-const char* password = "[redacted]";
-const char* mqtt_server = "192.168.0.17"; // use `hostname -I` on your server to determine
+// Replace with your network credentials
+const char* ssid = "ssid";
+const char* password = "password";
+
+const char* mqtt_server = "192.168.0.17"; // use hostname -I on your server to determine
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -13,27 +17,6 @@ String switch1;
 String strTopic;
 String strPayload;
 
-void setup_wifi() {
-
-  delay(10);
-  
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
 
 void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
@@ -72,20 +55,60 @@ void reconnect() {
     }
   }
 }
- 
-void setup()
-{
+
+void setup() {
   Serial.begin(115200);
-  setup_wifi(); 
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+   ArduinoOTA.setHostname("esp8266-luster");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH);
 }
- 
-void loop()
-{
+
+void loop() {
+  ArduinoOTA.handle();
+  delay(1);
+
   if (!client.connected()) {
     reconnect();
   }
